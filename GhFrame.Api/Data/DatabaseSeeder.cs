a6212446d81eda02375c51d6024de08b88dd61ba
eka <kia@DESKTOP-TCD7EKJ>
2025-05-08 14:08:36 +0700

    add inventory item seeder for testing

--- GhFrame.Api/Data/DatabaseSeeder.cs
+++ GhFrame.Api/Data/DatabaseSeeder.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using GhFrame.Api.Models;
+using GhFrame.Api.Abstractions.Services;
+using GhFrame.Api.Abstractions.Repositories;
+using GhFrame.Api.Models.Domain;
 
 namespace GhFrame.Api.Data;
 
@@ -44,8 +47,54 @@ public static class DatabaseSeeder
             await userManager.SetEmailAsync(adminUser, configuration["Admin:Email"]);
             await userManager.SetUserNameAsync(adminUser, configuration["Admin:Username"]);
             await userManager.CreateAsync(adminUser, configuration["Admin:Password"]!);
-            await userManager.AddToRoleAsync(adminUser, ApplicationRoles.Administrator);
-            
+            await userManager.AddToRoleAsync(adminUser, ApplicationRoles.Administrator);            
+        }
+
+        if(!await dbContext.Warehouses.AnyAsync()) {
+            Warehouse[] seedWareHouses = {
+                new Warehouse {
+                    Id = "GD001",
+                    Name = "Gudang Pusat"
+                },
+                new Warehouse {
+                    Id = "GD002",
+                    Name = "Gudang Toko"
+                },
+            };
+            dbContext.Warehouses.AddRange(seedWareHouses);
+
+            InventoryItem[] inventoryItems = {
+                new InventoryItem{
+                    Id = "IT001",
+                    WarehouseId = "GD001",
+                    Name = "Barang 1",
+                    Quantity = 1,
+                    ItemGroupName = "Group 1"
+                },
+                new InventoryItem{
+                    Id = "IT002",
+                    WarehouseId = "GD001",
+                    Name = "Barang 2",
+                    Quantity = 10,
+                    ItemGroupName = "Group 1"
+                },
+                new InventoryItem{
+                    Id = "IT003",
+                    WarehouseId = "GD001",
+                    Name = "Barang 3",
+                    Quantity = 5,
+                    ItemGroupName = "Group 1"
+                },
+                new InventoryItem{
+                    Id = "IT004",
+                    WarehouseId = "GD002",
+                    Name = "Samsung A1",
+                    Quantity = 1,
+                    ItemGroupName = "Group 2"
+                },
+            };
+            dbContext.InventoryItems.AddRange(inventoryItems);
+            dbContext.SaveChanges();
         }
 
     }
